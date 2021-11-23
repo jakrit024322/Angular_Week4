67a306077677c8e5d99936713cdab605b298d9c5
jakrit024322 <jakrit02432@gmail.com>
2021-11-23 16:54:12 +0700

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BS2';
+  n1: number = 5 
+  n2: number = 2
+  picName: string  = "https://images.pexels.com/photos/6190327/pexels-photo-6190327.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+
+  hi(name:string){
+    alert("Hello : " + name);
+  }
 }
+
